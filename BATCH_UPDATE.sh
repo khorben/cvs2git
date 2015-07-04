bc0bdcc156944707bf007d4f22c5119704f2cb73
Pierre Pronchery <khorben@defora.org>
2015-07-04 18:04:49 +0200

    Quote variables for safety

--- BATCH_UPDATE.sh
+++ BATCH_UPDATE.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir -p logs							|| return 2
-log=log.`date +%Y%m%d.%H%M`
-ln -sf $log logs/log
-make update > logs/$log 2>&1
+log=log.$(date +%Y%m%d.%H%M)
+ln -sf -- "$log" logs/log
+make update > "logs/$log" 2>&1
