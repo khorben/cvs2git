798cbd1f2b0f2b0401c19bcb142f72be054934b3
Pierre Pronchery <khorben@defora.org>
2016-08-17 23:50:51 +0200

    Test $? as an integer

--- rsync_completely.sh
+++ rsync_completely.sh
@@ -4,7 +4,7 @@ echo rsync --stats -az --delete "$1" "$2"
 rsync --stats -az --delete "$1" "$2" > rsync.log
 cat rsync.log
 grep -q 'Total transferred file size: 0 bytes$' rsync.log
-while [ "$?" != 0 ]; do
+while [ $? -ne 0 ]; do
 	echo "# retry rsync until no more file, after sleep 30."
 	sleep 30
 	echo rsync --stats -az --delete "$1" "$2"
